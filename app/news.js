83e5676f51cb3c8a9cc4d310afcc06ddc83d6e5b
swari <swari21@navgurukul.org>
2024-07-11 14:54:25 -0400

    completed

--- app/news.js
+++ app/news.js
@@ -1,20 +1,3 @@
-// import "./news.css";
-// import Newsarticle from './Newsarticle.js';
-// const News = (props) => {
-//   return (
-//     <div className="row">
-//       <Newsarticle newsData={props.article1} />
-//       <Newsarticle newsData={props.article2} />
-//       <Newsarticle newsData={props.article3} />
-//     </div>
-//   );
-// };
-
-// export default News;
-
-
-
-
 import React from 'react';
 import Newsarticle from "./Newsarticle.js";
 
